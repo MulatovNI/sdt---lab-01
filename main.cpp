f3c8c58fd9ad026a355021d031bdfea3a88fb209
Nikolay Mulatov <Nikolay Mulatov>
2016-09-16 11:47:39 +0400

    4.1

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@ int a;
 int b;
 int c;
 int z;
+int q;
 cout << "Vvedite 2 chisla." <<endl;
 cin >> a;
 cin >> b;
 c = a+b;
 z = a-b;
+q=a*b;
 cout << c << endl;
 cout << z << endl;
+cout << q << endl;
 return 0;
 }
